a5a58a342706e99522ee7b3053ce2e36ad6a5497
David Hernández <david.hmhernandez@gmail.com>
2014-11-07 13:33:32 +0100

    commit initial

---
+++ EjercicioFicheroAleatorio/src/es/torvall/LibraryManager.java
@@ -0,0 +1,5 @@
+package es.torvall;
+
+public class LibraryManager {
+
+}
---
+++ EjercicioFicheroAleatorio/src/es/torvall/test/LibraryManagerTest.java
@@ -0,0 +1,14 @@
+package es.torvall.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LibraryManagerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
