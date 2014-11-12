47bac4158954ad82679ea8130235323a9261b75b
David Hernández <david.hmhernandez@gmail.com>
2014-11-12 13:55:54 +0100

    Ejercicio 2 Terminado y Comentado

--- EjercicioFicheroAleatorio/src/es/torvall/Main.java
+++ EjercicioFicheroAleatorio/src/es/torvall/Main.java
@@ -1,12 +1,17 @@
 package es.torvall;
 
 public class Main {
+	final static String csvFile = "./resources/librosl.csv";
+	final static String randomFile = "./resources/librosl.bin";
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		LibraryManager lm = new LibraryManager();
+		lm.readCSV(csvFile);
+		lm.writeRandomFile(randomFile);
 
 	}
 
